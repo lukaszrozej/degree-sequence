9e6148e733f10deaf10d51562ee8b621be26c248
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-05-02 13:14:34 +0200

    Implement and test isDegreeSequence

--- jasmine/spec/degree-sequence-spec.js
+++ jasmine/spec/degree-sequence-spec.js
@@ -15,4 +15,20 @@ describe('isDegreeSequence', () => {
     })
   });
 
+  describe('when argument is not a degree sequence', () => {
+    it('should return false', () => {
+      expect(isDegreeSequence([1])).toBe(false);
+      expect(isDegreeSequence([1, 2, 3])).toBe(false);
+      expect(isDegreeSequence([1, 1, 1])).toBe(false);
+      expect(isDegreeSequence([5, 1, 2, 3, 1, 3])).toBe(false);
+    })
+  });
+
+  describe('when argument is a degree sequence', () => {
+    it('should return false', () => {
+      expect(isDegreeSequence([0])).toBe(true);
+      expect(isDegreeSequence([2, 2, 2, 2])).toBe(true);
+      expect(isDegreeSequence([5, 5, 5, 5, 5, 5])).toBe(true);
+    })
+  });
 });
--- src/js/degree-sequence.js
+++ src/js/degree-sequence.js
@@ -1,3 +1,16 @@
-function isDegreeSequence(sequence) {
-  if (!(sequence instanceof Array) || sequence.length === 0) return false;
+function isDegreeSequence(argument) {
+  if (!(argument instanceof Array) || argument.length === 0) return false;
+
+  const sequence = Array.from(argument).sort((a, b) => b - a);
+  const max = sequence[0];
+  const min = sequence[sequence.length - 1];
+
+  if (max >= sequence.length || min < 0) return false;
+
+  if (max === 0) return true;
+
+  return isDegreeSequence([
+    ...sequence.slice(1, max + 1).map(d => d - 1),
+    ...sequence.slice(max + 1)
+  ]);
 }
