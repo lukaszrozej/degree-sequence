7fe53ac0e7a7067f55f7e2d6ed3267d7dc70e2c7
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-05-02 09:36:54 +0200

    refacor: Check argument with 1 if

--- src/js/degree-sequence.js
+++ src/js/degree-sequence.js
@@ -1,4 +1,3 @@
 function isDegreeSequence(sequence) {
-  if (!(sequence instanceof Array)) return false;
-  if (sequence.length === 0) return false;
+  if (!(sequence instanceof Array) || sequence.length === 0) return false;
 }
