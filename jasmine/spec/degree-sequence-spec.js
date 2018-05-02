707b54c0864050fe35d89de5b875bcecd768461e
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-05-02 09:36:20 +0200

    Check argument

--- jasmine/spec/degree-sequence-spec.js
+++ jasmine/spec/degree-sequence-spec.js
@@ -2,4 +2,13 @@ describe('isDegreeSequence', () => {
   it('should be a function', () => {
     expect(isDegreeSequence).toEqual(jasmine.any(Function));
   });
+
+  it('should return false when argument is not an array', () => {
+    expect(isDegreeSequence()).toBe(false);
+  });
+
+  it('should return false when argument empty', () => {
+    expect(isDegreeSequence([])).toBe(false);
+  });
+
 });
--- src/js/degree-sequence.js
+++ src/js/degree-sequence.js
@@ -1,3 +1,4 @@
-function isDegreeSequence() {
-  
+function isDegreeSequence(sequence) {
+  if (!(sequence instanceof Array)) return false;
+  if (sequence.length === 0) return false;
 }
